08dfa42b6a688cd8efa43a665ff4d4f5b43f78a2
ana <anaclaudia1791@gmail.com>
2023-10-23 21:55:19 -0300

    Criado site de uma faculdade com flask e bootstrap

---
+++ src/app.py
@@ -0,0 +1,22 @@
+
+from flask import Flask, render_template
+
+
+app = Flask (__name__)
+
+
+@app.route("/")
+def home():
+    return render_template ('home.html')
+
+@app.route("/quem")
+def quem():
+    return render_template ('quem.html')
+
+@app.route("/contato")
+def contato():
+    return render_template ('contato.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
